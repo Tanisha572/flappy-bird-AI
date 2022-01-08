4b3d8f4a0dc64084feb07fb4894f6d57795e5980
Mukul Singh <mukulsingh1248@gmail.com>
2022-01-08 21:25:55 +0530

    added game progress and score

--- flappy_bird.py
+++ flappy_bird.py
@@ -4,6 +4,7 @@ import time
 import os
 import random
 
+pygame.font.init()
 
 # Defining the display window
 WIN_WIDTH, WIN_HEIGHT = 500, 800
@@ -17,11 +18,10 @@ BIRD_IMGS = [
     pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))),
     pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))
 ]
-
 PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
 BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
-
+STAT_FONT = pygame.font.SysFont("comicsans", 50)
 
 #frame rate for the game i.e. the #times the display will be refreshed in one second
 FPS = 30
@@ -146,25 +146,90 @@ class Pipe:
         win.blit(self.PIPE_TOP, (self.x, self.top))
         win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
     
+    #masks, in pygame, are used to detect pixel perfect collision
+    #masks basically monitors the position of the pixels against a transparent background
+    #each object on screen will be enclosed in a square and mask on that square will distinguish the 
+    #object pixels against the background pixels. 
+    #Overlapping of masks of two different objects will indicate the collision of the objects, rather
+    #than that of the squares. therefore, making the collision perfect in the user's prespective as well.
+
+    def collide(self, bird):
+        bird_mask = bird.get_mask()
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+
+        #offset of the bird from the top pipe and bottom pipe
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+
+        #finding the point of overlap between bird mask and bottom mask using the bottom_offset
+        b_point = bird_mask.overlap(bottom_mask, bottom_offset)
+        #finding the point of overlap between bird mask and top mask using the top_offset
+        t_point = bird_mask.overlap(top_mask, top_offset)
+
+        #if no overlap, overlap() returns none
+        if t_point or b_point :
+            #some collision occured
+            return True
+        #no collision occured
+        return False
+
+
+class Base:
+    VEL = 5     #defining same velocity as pipe so that they seem to move at the same pace
+    WIDTH = BASE_IMG.get_width()
+    IMG = BASE_IMG
+
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    def move(self):
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+
+        #cycling back the images as and when they go off window, giving an isllusion of endless base
+        #moving to the left
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+    #menthod to draw the base onto the display window
+    def draw(self, win):
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
 
 
 # Function to redraw the window for every iteration of the game loop
 
 #function to render the display window
-def draw_window(win, bird):
+def draw_window(win, bird, pipes, base, score):
     win.blit(BG_IMG, (0,0))     #blit() just renders the provided surface onto the display
+    
+    for pipe in pipes:
+        pipe.draw(win)
+    
+    text = STAT_FONT.render("Score: " + str(score), 1, (255,255,255))
+    win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
+
+    base.draw(win)
+
     bird.draw(win)
     pygame.display.update()
 
 
 def main():
-    bird = Bird(200, 200)
-    
-
+    bird = Bird(230, 350)
+    base = Base(730)
+    pipes = [Pipe(700)]
     #use clock object to set the tick rate i.e no. of frames per sec
     #prevents the game from using the system's speed and use this measure of time instead
-    
     clock = pygame.time.Clock()
+
+    score = 0
 
     run = True
     #the game loop for the pygame
@@ -174,8 +239,35 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
         
-        bird.move()
-        draw_window(win, bird)
+        # bird.move()
+        add_pipe = False
+        rem = []
+        for pipe in pipes:
+            if pipe.collide(bird):
+                pass
+
+            if pipe.x + pipe.PIPE_TOP.get_width() < 0:
+                rem.append(pipe)
+
+            if not pipe.passed and pipe.x < bird.x:
+                pipe.passed = True
+                add_pipe = True
+            
+            pipe.move()
+
+        if add_pipe:
+            score += 1
+            pipes.append(Pipe(700))
+
+        for r in rem:
+            pipes.remove(r)
+
+        # bird has hit ground
+        if bird.y + bird.img.get_height() > 730:
+            pass
+
+        base.move()
+        draw_window(win, bird, pipes, base, score)
 
     pygame.quit()
     quit()

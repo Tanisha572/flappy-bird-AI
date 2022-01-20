ee78b17e959ee8c1601b7f23d3119ef2e50d3ad0
Mukul Singh <mukulsingh1248@gmail.com>
2022-01-20 21:22:26 +0530

    training code done

--- flappy_bird.py
+++ flappy_bird.py
@@ -1,3 +1,4 @@
+from operator import ne
 import pygame
 import neat
 import time
@@ -24,7 +25,7 @@ BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png
 STAT_FONT = pygame.font.SysFont("comicsans", 50)
 
 #frame rate for the game i.e. the #times the display will be refreshed in one second
-FPS = 30
+FPS = 60
 
 
 # Making classes to represent each entity and define their behaviour in the game
@@ -206,7 +207,7 @@ class Base:
 # Function to redraw the window for every iteration of the game loop
 
 #function to render the display window
-def draw_window(win, bird, pipes, base, score):
+def draw_window(win, birds, pipes, base, score):
     win.blit(BG_IMG, (0,0))     #blit() just renders the provided surface onto the display
     
     for pipe in pipes:
@@ -217,12 +218,24 @@ def draw_window(win, bird, pipes, base, score):
 
     base.draw(win)
 
-    bird.draw(win)
+    for bird in birds:
+        bird.draw(win)
+
     pygame.display.update()
 
 
-def main():
-    bird = Bird(230, 350)
+def eval_fitness(genomes, config):
+    nets = []
+    ge = []
+    birds = []
+
+    for _, g in genomes:
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        nets.append(net)
+        birds.append(Bird(230,350))
+        g.fitness = 0
+        ge.append(g)
+
     base = Base(730)
     pipes = [Pipe(700)]
     #use clock object to set the tick rate i.e no. of frames per sec
@@ -238,42 +251,85 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                pygame.quit()
+                quit()
         
-        # bird.move()
+        pipe_index = 0
+        if len(birds) > 0:
+            if len(pipes) > 1 and birds[0].x > pipes[0].x + pipes[0].PIPE_TOP.get_width():
+                pipe_index = 1
+        else:
+            run = False
+            break
+
+        for x, bird in enumerate(birds):
+            bird.move()
+            ge[x].fitness += 0.1
+
+            output = nets[x].activate((bird.y, abs(bird.y - pipes[pipe_index].height), abs(bird.y - pipes[pipe_index].bottom)))
+
+            if output[0] > 0.5:
+                bird.jump()
+
         add_pipe = False
         rem = []
         for pipe in pipes:
-            if pipe.collide(bird):
-                pass
+            for x, bird in enumerate(birds):
+                if pipe.collide(bird):
+                    ge[x].fitness -= 1
+                    birds.pop(x)
+                    nets.pop(x)
+                    ge.pop(x)
+            
+                if not pipe.passed and pipe.x < bird.x:
+                    pipe.passed = True
+                    add_pipe = True
 
             if pipe.x + pipe.PIPE_TOP.get_width() < 0:
                 rem.append(pipe)
-
-            if not pipe.passed and pipe.x < bird.x:
-                pipe.passed = True
-                add_pipe = True
             
             pipe.move()
 
         if add_pipe:
             score += 1
+            # all birds which would have passed the pipes would remain in the lists
+            # so we incrent fitness fo all
+            for g in ge:
+                g.fitness += 5
             pipes.append(Pipe(700))
 
         for r in rem:
             pipes.remove(r)
 
-        # bird has hit ground
-        if bird.y + bird.img.get_height() > 730:
-            pass
+        for x, bird in enumerate(birds):
+            # check if bird has hit ground
+            if bird.y + bird.img.get_height() > 730 or bird.y < 0:
+                birds.pop(x)
+                nets.pop(x)
+                ge.pop(x)
 
         base.move()
-        draw_window(win, bird, pipes, base, score)
-
-    pygame.quit()
-    quit()
+        draw_window(win, birds, pipes, base, score)
 
 
+def run(config_path):
+    config = neat.config.Config(
+        neat.DefaultGenome,
+        neat.DefaultReproduction,
+        neat.DefaultSpeciesSet,
+        neat.DefaultStagnation,
+        config_path
+    )
+    p = neat.Population(config)
+
+    p.add_reporter(neat.StdOutReporter(True))
+    p.add_reporter(neat.StatisticsReporter())
+
+    winner = p.run(eval_fitness, 50)
+    print('\nBest genome:\n{!s}'.format(winner))
 
 if __name__ == "__main__":
-    main()
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config.txt")
+    run(config_path)
 
